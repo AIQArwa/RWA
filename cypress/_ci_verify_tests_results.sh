31794380f9d6470915ac755a88105ded0e443658
kamil <kamil.wrzyszcz@edulab.io>
2021-11-22 12:10:53 +0100

    fix for ci scripts showing wrong number of errors in case of a failed build

--- cypress/_ci_verify_tests_results.sh
+++ cypress/_ci_verify_tests_results.sh
@@ -12,7 +12,7 @@ source _ci_vars.sh
 NUMBER_OF_PARALLEL_LOG_ENTRIES=$(cat "${CI_PARALLEL_LOG_FILENAME}" | wc -l)
 NUMBER_OF_PARALLEL_LOG_ENTRIES=$((NUMBER_OF_PARALLEL_LOG_ENTRIES - 1))
 NUMBER_OF_SCENARIO_FILES=$(cat ${CI_SCENARIOS_LIST_FILENAME} | wc -l)
-NUMBER_OF_ERRORS=$(cat "${CI_PARALLEL_LOG_FILENAME}" | tail -n "$NUMBER_OF_SCENARIO_FILES" | awk '{print $7}' | grep '1' | wc -l)
+NUMBER_OF_ERRORS=$(cat "${CI_PARALLEL_LOG_FILENAME}" | tail -n "$NUMBER_OF_SCENARIO_FILES" | awk '{print $7}' | grep -v '0' | wc -l)
 
 echo
 echo "Number of parallel log entries: " $NUMBER_OF_PARALLEL_LOG_ENTRIES
@@ -20,10 +20,12 @@ echo "Number of scenarios:            " $NUMBER_OF_SCENARIO_FILES
 echo "Number of errors:               " $NUMBER_OF_ERRORS
 echo
 
-echo "0" > ${CI_FINAL_TEST_RESULT_FILENAME}
+CI_FINAL_TEST_RESULT=0
 
 if [ "$NUMBER_OF_ERRORS" -gt 0 ]; then
-    echo "1" > ${CI_FINAL_TEST_RESULT_FILENAME}
+    CI_FINAL_TEST_RESULT=1
 fi
+
+exit ${CI_FINAL_TEST_RESULT}
 
 # vim:ts=4:sw=4:et:syn=sh:
